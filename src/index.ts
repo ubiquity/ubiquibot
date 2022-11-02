10b358f278e5174ae52b391c9023b2524b5a612d
アレクサンダー.eth <gpg@pavlovcik.com>
2022-11-02 20:19:52 +0800

    feat: adds price tag to issue; still needs to clear old price

--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,6 @@
 import { Probot } from "probot";
-import { Payload } from "./interfaces/Payload";
-import { RecognizedProfits, RecognizedTimes } from "./interfaces/Recognized";
-import { calculateBountyPrice } from "./utils/calculateBountyPrice";
-import getLowestLabel from "./utils/getLowest";
+import { callbackOnAny } from "./utils/callbackOnAny";
 
 module.exports = function main(app: Probot) {
   app.onAny(callbackOnAny);
 };
-
-async function callbackOnAny(event: any) {
-  const payload = event.payload as Payload;
-
-  if (event.name != "issues") return;
-  if (payload.issue.number != 9) return;
-  if (payload.action != "labeled" && payload.action != "unlabeled") return;
-
-  const labels = payload.issue.labels;
-  const issueTimes = labels.filter((label) => label.name.startsWith("Time:"));
-  const issueProfits = labels.filter((label) => label.name.startsWith("Profit:"));
-
-  if (!issueTimes.length) {
-    throw new Error(`No times found in issue labels ${JSON.stringify(labels.map((label) => label.name))}`);
-  }
-  if (!issueProfits.length) {
-    throw new Error(`No profits found in issue labels ${JSON.stringify(labels.map((label) => label.name))}`);
-  }
-
-  const lowestTime = getLowestLabel(issueTimes, RecognizedTimes);
-  const lowestProfit = getLowestLabel(issueProfits, RecognizedProfits);
-  const bountyPrice = calculateBountyPrice(lowestTime, lowestProfit);
-
-  console.log(bountyPrice, `USDC`);
-}
