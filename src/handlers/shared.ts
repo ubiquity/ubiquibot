7a0049e6a61b42f2deedbb2a73a2105b04244c53
0xCodercrane <108444211+0xcodercrane@users.noreply.github.com>
2022-11-25 18:50:24 +0800

    add serverless

--- src/handlers/shared.ts
+++ src/handlers/shared.ts
@@ -2,10 +2,9 @@ import { getBotContext } from "../bindings";
 import { ActionHandler } from "../types";
 
 export const nullHandler: ActionHandler = async (): Promise<void> => {
+  // ToDo: This is just a null handler to do nothing. just needed for mockup
+  // This would be replaced with the meaningful handler once its feature determined
 
-    // ToDo: This is just a null handler to do nothing. just needed for mockup
-    // This would be replaced with the meaningful handler once its feature determined
-
-    const {log} = getBotContext();
-    log.debug(`Running handler, name: ${nullHandler.name}`);
-}
+  const { log } = getBotContext();
+  log.debug(`Running handler, name: ${nullHandler.name}`);
+};
--- src/types/handlers.ts
+++ src/types/handlers.ts
@@ -4,8 +4,8 @@ export type PostActionHandler = ActionHandler;
 
 /**
  * @dev A set of handlers to do a pre/main/post action for a given action
- * @param pre - An array of pre-action handlers, it consists of a small piece of handlers. 
- *  If pre action isn't needed, you can just leave it empty 
+ * @param pre - An array of pre-action handlers, it consists of a small piece of handlers.
+ *  If pre action isn't needed, you can just leave it empty
  * @param action - An array of auction handlers, it consists of a small piece of handlers
  * @param post - An array of post action handlers, it consists of a small piece of handlers
  */
