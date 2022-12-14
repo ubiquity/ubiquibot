9bfed64f9a763fca843160623b06fccf220015f4
0xCodercrane <108444211+0xcodercrane@users.noreply.github.com>
2022-12-15 01:34:25 +0800

    try catch

--- src/helpers/label.ts
+++ src/helpers/label.ts
@@ -1,4 +1,4 @@
-import { getBotContext } from "../bindings";
+import {  getBotContext } from "../bindings";
 import { COLORS } from "../configs";
 import { Payload } from "../types";
 
@@ -21,11 +21,16 @@ export const listLabelsForRepo = async (): Promise<string[]> => {
 export const createLabel = async (name: string): Promise<void> => {
   const context = getBotContext();
   const payload = context.payload as Payload;
+  try {
+    await context.octokit.rest.issues.createLabel({
+      owner: payload.repository.owner.login,
+      repo: payload.repository.name,
+      name,
+      color: COLORS.price,
+    });
+  
+  } catch (err: unknown) {
+    context.log.debug(`Error creating a label: ${name}. Is it already there?`);
+  }
 
-  await context.octokit.rest.issues.createLabel({
-    owner: payload.repository.owner.login,
-    repo: payload.repository.name,
-    name,
-    color: COLORS.price,
-  });
 };
