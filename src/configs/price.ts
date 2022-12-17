26b8598902435b997175c5333a1121e60cad62c1
アレクサンダー.eth <gpg@pavlovcik.com>
2022-12-18 02:39:09 +0800

    fix: USDC to USD because sometimes we pay DAI

--- src/configs/price.ts
+++ src/configs/price.ts
@@ -6,44 +6,44 @@ export const DefaultPriceConfig: PriceConfig = {
     {
       name: "Time: <1 Day",
       weight: 1,
-      target: "Price: 100-1000 USDC",
+      target: "Price: 100-1000 USD",
     },
     {
       name: "Time: <1 Week",
       weight: 2,
-      target: "Price: 200-2000 USDC",
+      target: "Price: 200-2000 USD",
     },
     {
       name: "Time: <2 Weeks",
       weight: 3,
-      target: "Price: 300-3000 USDC",
+      target: "Price: 300-3000 USD",
     },
     {
       name: "Time: <1 Month",
       weight: 4,
-      target: "Price: 400-4000 USDC",
+      target: "Price: 400-4000 USD",
     },
   ],
   profitLabels: [
     {
       name: "Profit: <10%",
       weight: 1,
-      target: "Price: 100-400 USDC",
+      target: "Price: 100-400 USD",
     },
     {
       name: "Profit: <50%",
       weight: 5,
-      target: "Price: 500-2000 USDC",
+      target: "Price: 500-2000 USD",
     },
     {
       name: "Profit: <100%",
       weight: 10,
-      target: "Price: 1000-4000 USDC",
+      target: "Price: 1000-4000 USD",
     },
     {
       name: "Profit: 100%+",
       weight: 20,
-      target: "Price: 2000-8000 USDC",
+      target: "Price: 2000-8000 USD",
     },
   ],
 };
