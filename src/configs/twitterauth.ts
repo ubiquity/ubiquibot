726fa7aab7358aba7b928b34d7b59028eef948f7
MANUEL RIOS <34518489+ManyRios@users.noreply.github.com>
2022-12-31 10:08:52 -0400

    Updating enviorement var

--- src/configs/twitterauth.ts
+++ src/configs/twitterauth.ts
@@ -1,6 +1,5 @@
 // Credentials for twitter account 
-export const TW_PORT: string = process.env.PORT || "";
-export const TW_API_KEY: string = process.env.API_KEY || "";
-export const TW_API_KET_SECRET: string = process.env.API_KET_SECRET || "";
-export const TW_ACCESS_TOKEN: string = process.env.ACCESS_TOKEN || "";
-export const TW_ACCESS_TOKEN_SECRET: string = process.env.ACCESS_TOKEN_SECRET || "";
+export const TW_API_KEY: string = process.env.TW_API_KEY || "";
+export const TW_API_KET_SECRET: string = process.env.TW_API_KET_SECRET || "";
+export const TW_ACCESS_TOKEN: string = process.env.TW_ACCESS_TOKEN || "";
+export const TW_ACCESS_TOKEN_SECRET: string = process.env.TW_ACCESS_TOKEN_SECRET || "";
