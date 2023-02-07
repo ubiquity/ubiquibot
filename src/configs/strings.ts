2dbac06567ab3b5662dccadf90ac245418a364a0
0xCodercrane <108444211+0xcodercrane@users.noreply.github.com>
2023-02-07 10:58:13 +0800

    add ask_update string to global

--- src/configs/strings.ts
+++ src/configs/strings.ts
@@ -1,3 +1,4 @@
 export const GLOBAL_STRINGS = {
   unassign_comment: "Releasing the bounty back to dev pool because the allocated duration already ended!",
+  ask_update: "Do you have any updates?",
 };
