a0fc4140ff8949e1afaf1d6b42cee70b3db70ebd
0xCodercrane <108444211+0xcodercrane@users.noreply.github.com>
2023-02-14 17:13:36 +0800

    incorrect use of adapter

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { Probot } from "probot";
 import { bindEvents } from "./bindings";
-import { fetchGlobalStorage } from "./global/globalStorage";
+// import { fetchGlobalStorage } from "./global/globalStorage";
 
 const UBIQUITY = `
   _|    _|  _|_|_|    _|_|_|    _|_|      _|    _|  _|_|_|  _|_|_|_|_|  _|      _|
@@ -14,5 +14,5 @@ const UBIQUITY = `
 export default function main(app: Probot) {
   console.log(UBIQUITY);
   app.onAny(bindEvents as any);
-  fetchGlobalStorage();
+  // fetchGlobalStorage();
 }
