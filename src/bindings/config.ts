bf0c2649239820a05a587776ae985d752647253c
0xCodercrane <108444211+0xcodercrane@users.noreply.github.com>
2023-02-15 11:48:18 +0800

    use ms

--- src/bindings/config.ts
+++ src/bindings/config.ts
@@ -1,7 +1,7 @@
 import { BotConfig, BotConfigSchema } from "../types";
 import fs from "fs";
 import path from "path";
-import { parse } from "tinyduration";
+import ms from "ms";
 
 import { DefaultPriceConfig, DEFAULT_BOT_DELAY, DEFAULT_DISQUALIFY_TIME, DEFAULT_FOLLOWUP_TIME } from "../configs";
 import { ajv } from "../utils";
@@ -11,18 +11,6 @@ const getFallback = (value: string, target: string) => {
   return value;
 };
 
-const convertFromDuration = (duration: string): number => {
-  const durationObj = parse(duration);
-
-  const durationInSecs =
-    365 * 24 * 3600 * (durationObj.years ?? 0) +
-    30 * 24 * 3600 * (durationObj.months ?? 0) +
-    24 * 3600 * (durationObj.days ?? 0) +
-    3600 * (durationObj.hours ?? 0) +
-    60 * (durationObj.minutes ?? 0) +
-    (durationObj.seconds ?? 0);
-  return durationInSecs * 1000;
-};
 export const loadConfig = async (): Promise<BotConfig> => {
   let configFile: any = {};
   const configFilePath = path.join(__dirname, "./config.json");
@@ -37,8 +25,8 @@ export const loadConfig = async (): Promise<BotConfig> => {
       priorityLabels: configFile.priorityLabels ?? DefaultPriceConfig.priorityLabels,
     },
     unassign: {
-      followUpTime: convertFromDuration(process.env.FOLLOW_UP_TIME || DEFAULT_FOLLOWUP_TIME),
-      disqualifyTime: convertFromDuration(process.env.DISQUALIFY_TIME || DEFAULT_DISQUALIFY_TIME),
+      followUpTime: ms(process.env.FOLLOW_UP_TIME || DEFAULT_FOLLOWUP_TIME),
+      disqualifyTime: ms(process.env.DISQUALIFY_TIME || DEFAULT_DISQUALIFY_TIME),
     },
     supabase: {
       url: process.env.SUPABASE_PROJECT_URL ?? "",
