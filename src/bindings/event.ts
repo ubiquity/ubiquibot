58abb83a4bfe43bcbe000d737482ffec15b92196
0xCodercrane <108444211+0xcodercrane@users.noreply.github.com>
2023-02-15 16:51:40 +0800

    config logging

--- src/bindings/event.ts
+++ src/bindings/event.ts
@@ -32,6 +32,7 @@ export const bindEvents = async (context: Context): Promise<void> => {
   // Load config
   log.info("Loading config from .env...");
   botConfig = await loadConfig();
+  log.info(`Config loaded! config: ${JSON.stringify({ price: botConfig.price, unassign: botConfig.unassign, git: botConfig.git })}`);
 
   // Create adapters for telegram, supabase, twitter, discord, etc
   log.info("Creating adapters for supabase, telegram, twitter, etc...");
