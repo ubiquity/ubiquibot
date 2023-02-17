1ed9d0d43a477ad7d4827e4a334e581756085689
0xCodercrane <108444211+0xcodercrane@users.noreply.github.com>
2023-02-18 00:39:14 +0800

    null check

--- src/handlers/comment/handlers/assign.ts
+++ src/handlers/comment/handlers/assign.ts
@@ -1,5 +1,4 @@
 import { getBotConfig, getBotContext } from "../../../bindings";
-import { BountyAccount } from "../../../configs";
 import { addAssignees, addCommentToIssue, getCommentsOfIssue, removeAssignees } from "../../../helpers";
 import { Payload, LabelItem, Comment } from "../../../types";
 import { deadLinePrefix } from "../../shared";
@@ -17,7 +16,7 @@ export const assign = async (body: string) => {
   log.info(`Received '/assign' command from user: ${payload.sender.login}`);
   const issue_number = (_payload as Payload).issue?.number;
   const _assignees = payload.issue?.assignees;
-  const assignees = _assignees ? _assignees?.filter((i) => ![BountyAccount].includes(i.login)) : [];
+  const assignees = _assignees ?? [];
 
   // get the time label from the `labels`
   const labels = payload.issue?.labels;
@@ -55,17 +54,19 @@ export const assign = async (body: string) => {
   const endDate = new Date(curDateInMillisecs + duration * 1000);
   const commit_msg = `@${payload.sender.login} ${deadLinePrefix} ${endDate.toLocaleDateString("en-us")}`;
 
-  log.info(`Creating an issue comment`, { commit_msg });
+  log.info(`Creating an issue comment: ${commit_msg}`);
 
   if (assignees.length > 0) {
     const filteredAssignees = assignees.filter((i) => i != payload.sender.login);
     if (filteredAssignees.length > 0) {
       // remove assignees from the issue
+      log.info("Removing the previous assignees...");
       await removeAssignees(issue_number!, assignees);
     }
   }
 
   if (!assignees.includes(payload.sender.login)) {
+    log.info(`Adding the assignee: ${payload.sender.login}`);
     // assign default bounty account to the issue
     await addAssignees(issue_number!, [payload.sender.login]);
   }
@@ -73,8 +74,8 @@ export const assign = async (body: string) => {
   // double check whether the assign message has been already posted or not
   const issue_comments = await getCommentsOfIssue(issue_number!);
   const comments = issue_comments.sort((a: Comment, b: Comment) => new Date(b.created_at).getTime() - new Date(a.created_at).getTime());
-  const latest_comment = comments[0].body;
-  if (commit_msg != latest_comment) {
+  const latest_comment = comments.length > 0 ? comments[0].body : undefined;
+  if (latest_comment && commit_msg != latest_comment) {
     await addCommentToIssue(commit_msg, issue_number!);
   }
 };
