eeb09227bd6cd6164ad978e431112aabdcf8e8b0
0xCodercrane <108444211+0xcodercrane@users.noreply.github.com>
2023-02-27 23:04:40 +0800

    fix: remove testing purpose code

--- src/main.ts
+++ src/main.ts
@@ -12,8 +12,5 @@ const UBIQUITY = `
 
 export default function main(app: Probot) {
   console.log(UBIQUITY);
-  // app.onAny(bindEvents as any);
-  app.on("issue_comment.created", bindEvents);
-  app.on("issue_comment.edited", bindEvents);
-  app.on("issues.closed", bindEvents);
+  app.onAny(bindEvents as any);
 }
