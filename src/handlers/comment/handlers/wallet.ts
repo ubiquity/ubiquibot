2f846c274f4f39de711cdb80926a4812d930e2cb
0xCodercrane <108444211+0xcodercrane@users.noreply.github.com>
2023-03-02 14:12:37 +0800

    simple

--- src/handlers/comment/handlers/wallet.ts
+++ src/handlers/comment/handlers/wallet.ts
@@ -1,25 +1,21 @@
-import { ethers } from "ethers";
 import { upsertWalletAddress } from "../../../adapters/supabase";
 import { getBotContext } from "../../../bindings";
 import { addCommentToIssue, resolveAddress } from "../../../helpers";
 import { Payload } from "../../../types";
+import { IssueCommentCommands } from "../commands";
 
+// Extracts ensname from raw text.
 const extractEnsName = (text: string): string | undefined => {
   // Define a regular expression to match ENS names
-  const ensRegex = /^([a-z0-9](?:-?[a-z0-9]){0,61}[a-z0-9])?\.([a-z]{2,})(\.eth)?$/i;
+  const ensRegex = /^(?=.{3,40}$)([a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z]{2,}$/gm;
 
   // Find the first match of the regular expression in the input text
   const match = text.match(ensRegex);
+  console.log("> match: ", match);
 
   if (match) {
-    // Construct the full ENS name from the matched groups
-    const [label, tld, eth] = match;
-    const ensName = `${label}${tld || eth || ""}`;
-
-    // Validate the ENS name using ethers.js
-    if (ethers.utils.isAddress(ensName)) {
-      return ensName.toLowerCase();
-    }
+    const ensName = match[0];
+    return ensName.toLowerCase();
   }
 
   return undefined;
@@ -33,8 +29,8 @@ export const registerWallet = async (body: string): Promise<void> => {
   const regexForAddress = /(0x[a-fA-F0-9]{40})/g;
   const addressMatches = body.match(regexForAddress);
   let address = addressMatches ? addressMatches[0] : undefined;
-  const ensName = extractEnsName(body);
-  log.info(`Received '/wallet' command from user: ${sender}, body: ${body}`);
+  const ensName = extractEnsName(body.replace(IssueCommentCommands.WALLET, "").trim());
+  log.info(`Received '/wallet' command from user: ${sender}, body: ${body}, ${ensName}`);
 
   if (!address && !ensName) {
     log.info("Skipping to register a wallet address because both address/ens doesn't exist");
@@ -48,6 +44,7 @@ export const registerWallet = async (body: string): Promise<void> => {
       log.info(`Resolving address from Ens name failed, EnsName: ${ensName}`);
       return;
     }
+    log.info(`Resolved address from Ens name: ${ensName}, address: ${address}`);
   }
 
   await upsertWalletAddress(sender, address);
--- src/helpers/ens.ts
+++ src/helpers/ens.ts
@@ -1,36 +1,15 @@
 import { ethers } from "ethers";
 import { getBotConfig } from "../bindings";
 
-const EnsRegistryMap: Record<string, string> = {
-  "1": "0x00000000000C2E074eC69A0dFb2997BA6C7d2e1e",
-};
-
 /**
  * Gets the Ethereum address associated with an ENS (Ethereum Name Service) name
  * @param ensName - The ENS name, i.e. alice12345.crypto
  */
-export const resolveAddress = async (ensName: string): Promise<string | undefined> => {
+export const resolveAddress = async (ensName: string): Promise<string | null> => {
   const {
-    payout: { chainId, rpc },
+    payout: { rpc },
   } = getBotConfig();
-  let address;
-  if (!EnsRegistryMap[chainId.toString()]) return undefined;
-
-  const ensRegistryAddress = EnsRegistryMap[chainId.toString()];
   const provider = new ethers.providers.JsonRpcProvider(rpc);
-
-  const ensRegistryAbi = ["function resolver(bytes32 tld) view returns (address)"];
-  const ensRegistry = new ethers.Contract(ensRegistryAddress, ensRegistryAbi, provider);
-  const tld = `.${ensName.split(".")[1]}`;
-  const tldHash = ethers.utils.namehash(tld);
-  const resolverAddress = await ensRegistry.resolver(tldHash);
-
-  const ensResolverAbi = ["function addr(bytes32 node) view returns (address)"];
-  const ensResolver = new ethers.Contract(resolverAddress, ensResolverAbi, provider);
-
-  // Get the Ethereum address associated with the ENS name
-  const node = ethers.utils.namehash(ensName);
-  address = await ensResolver.addr(node);
-
+  const address = await provider.resolveName(ensName);
   return address;
 };
