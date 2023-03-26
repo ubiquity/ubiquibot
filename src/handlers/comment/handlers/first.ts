3864db206a83567963efdeb332bf5a81273403c6
Steveantor <109317559+Steveantor@users.noreply.github.com>
2023-03-26 15:57:53 +0000

    fix: first comment logic

--- src/handlers/comment/handlers/first.ts
+++ src/handlers/comment/handlers/first.ts
@@ -13,9 +13,20 @@ export const verifyFirstCheck = async (): Promise<void> => {
       q: `is:issue repo:${payload.repository.owner.login}/${payload.repository.name} commenter:${payload.sender.login}`,
       per_page: 2,
     });
-    if (response.data.total_count >= 2) {
-      const msg = `${COMMAND_INSTRUCTIONS}\n@${payload.sender.login}`;
-      await addCommentToIssue(msg, payload.issue!.number);
+    if (response.data.total_count === 1) {
+      //continue_first_search
+      const resp = await context.octokit.rest.issues.listComments({
+        issue_number: response.data.items[0].number,
+        owner: payload.repository.owner.login,
+        repo: payload.repository.name,
+        per_page: 100,
+      });
+      const isFirstComment = resp.data.filter((item) => item.user?.login === payload.sender.login).length === 1;
+      if (isFirstComment) {
+        //first_comment
+        const msg = `${COMMAND_INSTRUCTIONS}\n@${payload.sender.login}`;
+        await addCommentToIssue(msg, payload.issue!.number);
+      }
     }
   } catch (error: unknown) {
     logger.info(`First comment verification failed, reason: ${error}`);
