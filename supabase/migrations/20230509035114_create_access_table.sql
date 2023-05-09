f388b49cfecd9d349d4f2098a53c457ef7587304
DevPanther <techicovery@gmail.com>
2023-05-09 08:41:07 +0100

    feat: added db sql

---
+++ supabase/migrations/20230509035114_create_access_table.sql
@@ -0,0 +1,10 @@
+CREATE TABLE weekly (
+    user_name character varying(255) NOT NULL PRIMARY KEY,
+    priority_access  boolean,
+    time_access boolean,
+    price_access boolean,
+    multiplier_access boolean,
+    repository text,
+    created_at text,
+    updated_at text
+);
