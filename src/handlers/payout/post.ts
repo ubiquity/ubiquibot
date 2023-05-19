d442b65b9845b2d9dfee87f7f2f1d8e3a127588f
0xcodercrane <108444211+0xcodercrane@users.noreply.github.com>
2023-05-19 21:44:44 +0800

    chore: more precise name

--- src/handlers/payout/post.ts
+++ src/handlers/payout/post.ts
@@ -7,7 +7,7 @@ import { Payload } from "../../types";
  * Incentivize the contributors based on their contribution.
  * The default formula has been defined in https://github.com/ubiquity/ubiquibot/issues/272
  */
-export const incentiveContribution = async () => {
+export const incentivizeComments = async () => {
   const logger = getLogger();
   const {
     mode: { incentiveMode },
