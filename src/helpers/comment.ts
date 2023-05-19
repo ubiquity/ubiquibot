b192f068a1b9b79f1cab749b7116105c2ae6b8b5
0xcodercrane <108444211+0xcodercrane@users.noreply.github.com>
2023-05-19 11:32:59 +0800

    feat: traverse impl

--- src/helpers/comment.ts
+++ src/helpers/comment.ts
@@ -11,15 +11,44 @@ export const MarkdownItem = {
 } as const;
 export type MarkdownItem = (typeof MarkdownItem)[keyof typeof MarkdownItem];
 
+type MdastNode = {
+  type: string;
+  value: string;
+  children: MdastNode[];
+};
+
+const traverse = (node: MdastNode): Record<string, string[]> => {
+  let result: Record<string, string[]> = {};
+  if (!result[node.type]) {
+    result[node.type] = [];
+  }
+
+  result[node.type].push(node.value);
+
+  if (node.children) {
+    node.children.forEach((child) => traverse(child));
+  }
+
+  return result;
+};
+
 export const parseComments = async (comments: string[]) => {
+  let result: Record<string, string[]> = {};
   for (const comment of comments) {
     const tree = fromMarkdown(comment, {
       extensions: [gfm()],
       mdastExtensions: [gfmFromMarkdown()],
     });
 
-    tree.children;
-
-    console.log(JSON.stringify(tree));
+    const parsedContent = traverse(tree as MdastNode);
+    for (const key of Object.keys(parsedContent)) {
+      if (Object.keys(result).includes(key)) {
+        result[key].push(...parsedContent[key]);
+      } else {
+        result[key] = parsedContent[key];
+      }
+    }
   }
+
+  console.log(result);
 };
