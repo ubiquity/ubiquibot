47e9e3ef8b7c2b5101db0a272c7a14d26ba5d701
HARALD <danddycandy@gmail.com>
2023-06-04 21:19:01 +0900

    feat: put review comment after the payment

--- src/handlers/comment/handlers/index.ts
+++ src/handlers/comment/handlers/index.ts
@@ -43,11 +43,11 @@ export const issueClosedCallback = async (): Promise<void> => {
   const issue = (_payload as Payload).issue;
   try {
     const comment = await handleIssueClosed();
+    await addCommentToIssue(comment!, issue!.number);
     await addCommentToIssue(
       `If you enjoy the DevPool experience, please follow <a href="https://github.com/ubiquity">Ubiquity on GitHub</a> and star <a href="https://github.com/ubiquity/devpool-directory">this repo</a> to show your support. It helps a lot!`,
       issue!.number
     );
-    return await addCommentToIssue(comment!, issue!.number);
   } catch (err: any) {
     return await addCommentToIssue("Error: " + err.message, issue!.number);
   }
