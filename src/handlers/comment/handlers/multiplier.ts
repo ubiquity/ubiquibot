5beb709011b5595763bf3bfd419ec3859e0474bd
0xCodercrane <108444211+0xcodercrane@users.noreply.github.com>
2023-07-13 17:02:33 +0800

    fix: logging

--- src/handlers/comment/handlers/multiplier.ts
+++ src/handlers/comment/handlers/multiplier.ts
@@ -66,11 +66,7 @@ export const multiplier = async (body: string) => {
       }
     }
 
-    logger.info(
-      `Upserting to the wallet table, username: ${username}, bountyMultiplier: ${bountyMultiplier}, reason: ${reason}, matches: ${matches.map((it) =>
-        it.toString()
-      )}`
-    );
+    logger.info(`Upserting to the wallet table, username: ${username}, bountyMultiplier: ${bountyMultiplier}, reason: ${reason}}`);
 
     await upsertWalletMultiplier(username, bountyMultiplier?.toString(), reason);
     return `Successfully changed the payout multiplier for @${username} to ${bountyMultiplier}. The reason ${
