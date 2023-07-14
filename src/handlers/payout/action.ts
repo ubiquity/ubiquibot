85c613ca1607d84ad687e80105d01133c678ae75
me505 <62057938+me505@users.noreply.github.com>
2023-07-14 19:56:16 +0530

    Update src/handlers/payout/action.ts
    Co-authored-by: 0xCodercrane <108444211+0xcodercrane@users.noreply.github.com>

--- src/handlers/payout/action.ts
+++ src/handlers/payout/action.ts
@@ -74,10 +74,8 @@ export const handleIssueClosed = async () => {
   logger.info(`Posting a payout url to the issue, url: ${payoutUrl}`);
   const comment = `### [ **[ CLAIM ${priceInEth} ${tokenSymbol.toUpperCase()} ]** ](${payoutUrl})\n` + "```" + shortenRecipient + "```";
   const comments = await getAllIssueComments(issue.number);
-  //const commentContents = comments.map((i) => i.body);
-  const permitComments = comments.filter((content) => content.body.includes("https://pay.ubq.fi?claim="));
-  const exist = permitComments.filter((content) => content.user.type == "Bot")
-  if (exist.length) {
+  const permitComments = comments.filter((content) => content.body.includes("https://pay.ubq.fi?claim=") && content.user.type == UserType.Bot);
+    if (permitComments.length > 0) {
     logger.info(`Skip to generate a permit url because it has been already posted`);
     return `Permit generation skipped because it was already posted to this issue.`;
   }
