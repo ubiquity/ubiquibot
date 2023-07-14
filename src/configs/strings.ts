7117195646fd5653e23be1248b20f010833c83e1
Wanna C The Future! <danddycandy@gmail.com>
2023-07-14 14:39:51 +0900

    Update src/configs/strings.ts
    Co-authored-by: アレクサンダー.eth <4975670+pavlovcik@users.noreply.github.com>

--- src/configs/strings.ts
+++ src/configs/strings.ts
@@ -1,5 +1,5 @@
 export const GLOBAL_STRINGS = {
   unassignComment: "Releasing the bounty back to dev pool because the allocated duration already ended!",
   askUpdate: "Do you have any updates",
-  assignCommandDisabledComment: "`/assign` command is disabled.",
+  assignCommandDisabledComment: "The `/assign` command is disabled for this repository.",
 };
