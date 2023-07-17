3f45065f7c1f00a94aac4eb551ff2c37de78ce72
HARALD <danddycandy@gmail.com>
2023-07-18 00:32:40 +0900

    feat: fixed config yaml and default message

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -88,7 +88,7 @@ export const getPromotionComment = (parsedRepo: WideRepoConfig | undefined, pars
   } else if (parsedOrg && parsedOrg["promotion-comment"] && typeof parsedOrg["promotion-comment"] === "string") {
     return parsedOrg["promotion-comment"];
   } else {
-    return "";
+    return "\n<h6>If you enjoy the DevPool experience, please follow <a href='https://github.com/ubiquity'>Ubiquity on GitHub</a> and star <a href='https://github.com/ubiquity/devpool-directory'>this repo</a> to show your support. It helps a lot!</h6>";
   }
 };
 
