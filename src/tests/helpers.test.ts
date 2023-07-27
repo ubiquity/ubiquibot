3b8b648cd16c0bcae31088b7f4281ede2a25efee
Paul <web3samuraidev@gmail.com>
2023-07-27 07:00:05 +0000

    test: payout chainId test

--- src/tests/helpers.test.ts
+++ src/tests/helpers.test.ts
@@ -6,13 +6,24 @@ jest.mock("../configs");
 describe("Helpers", () => {
   it("should return the payout config for a valid chainId", () => {
     const chainId = 1;
-    const expectedConfig = {
+    const mainnetConfig = {
       rpc: DEFAULT_RPC_ENDPOINT,
       paymentToken: "0x6B175474E89094C44Da98b954EedeAC495271d0F", //DAI
     };
 
     const result = getPayoutConfigByChainId(chainId);
 
-    expect(result).toEqual(expectedConfig);
+    expect(result).toEqual(mainnetConfig);
+  });
+
+  it("should return the payout config for valid GNOSIS ChainID", () => {
+    const chainId = 100;
+    const gnosisConfig = {
+      rpc: "https://rpc.gnosischain.com",
+      paymentToken: "0xe91D153E0b41518A2Ce8Dd3D7944Fa863463a97d", //WXDAI
+    };
+
+    const result = getPayoutConfigByChainId(chainId);
+    expect(result).toEqual(gnosisConfig);
   });
 });
