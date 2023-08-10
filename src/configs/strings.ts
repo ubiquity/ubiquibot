15792939aa6ef6549551fb6eb0034e8a121ed4af
FUTURE <danddycandy@gmail.com>
2023-08-10 17:07:08 +0900

    Update src/configs/strings.ts
    Co-authored-by: アレクサンダー.eth <4975670+pavlovcik@users.noreply.github.com>

--- src/configs/strings.ts
+++ src/configs/strings.ts
@@ -2,6 +2,6 @@ export const GLOBAL_STRINGS = {
   unassignComment: "Releasing the bounty back to dev pool because the allocated duration already ended!",
   askUpdate: "Do you have any updates",
   assignCommandDisabledComment: "The `/assign` command is disabled for this repository.",
-  skipPriceLabelGenerationComment: "Price label generation skipped since it is identified as parent issue.",
+  skipPriceLabelGenerationComment: "Pricing is disabled on parent issues.",
   ignorStartCommandForParentIssuesComment: "The `/start` command is disabled for parent issues.",
 };
