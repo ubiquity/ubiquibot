f13face7ce04ae9480a4a219d79f7a8ec9749623
FUTURE <danddycandy@gmail.com>
2023-08-10 17:07:17 +0900

    Update src/configs/strings.ts
    Co-authored-by: アレクサンダー.eth <4975670+pavlovcik@users.noreply.github.com>

--- src/configs/strings.ts
+++ src/configs/strings.ts
@@ -3,5 +3,5 @@ export const GLOBAL_STRINGS = {
   askUpdate: "Do you have any updates",
   assignCommandDisabledComment: "The `/assign` command is disabled for this repository.",
   skipPriceLabelGenerationComment: "Pricing is disabled on parent issues.",
-  ignorStartCommandForParentIssuesComment: "The `/start` command is disabled for parent issues.",
+  ignoreStartCommandForParentIssueComment: "Please select a child issue from the specification checklist to work on. The `/start` command is disabled on parent issues.",
 };
