6268d44df93135fffb6a129ead2f80142b33e5bd
EtherealGlow <aarjavjain092@gmail.com>
2023-08-12 09:48:27 +0000

    fix: query regex

--- src/handlers/comment/handlers/query.ts
+++ src/handlers/comment/handlers/query.ts
@@ -19,9 +19,9 @@ export const query = async (body: string) => {
     return `Skipping '/query' because of no issue instance`;
   }
 
-  const regex = /(?<=\/query @)\w+/g
+  const regex = /^\/query\s+(@\w+)\s?$/;
   const matches = body.match(regex);
-  const user = matches?.[1];
+  const user = matches?.[1].substring(1);
 
   if (user) {
     const walletInfo = await getWalletInfo(user, id?.toString());
