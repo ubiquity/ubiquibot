3b2642aca6cd0c7b2c29b9bdbda5858a9723660a
byteballet <shaorun1010@gmail.com>
2023-08-15 00:03:34 -0700

    fix: store chatgpt configs to .env and applied new strategy

--- src/handlers/issue/pre.ts
+++ src/handlers/issue/pre.ts
@@ -1,33 +1,52 @@
-import { extractImportantWords, countIncludedWords, upsertCommentToIssue, listIssuesForRepo } from "../../helpers";
+import { extractImportantWords, upsertCommentToIssue, measureSimilarity } from "../../helpers";
 import { getBotContext } from "../../bindings";
 import { Payload } from "../../types";
+import { logger } from "ethers";
 
 export const findDuplicateOne = async () => {
-  const { payload: _payload } = getBotContext();
-  const issue = (_payload as Payload).issue;
+  const context = getBotContext();
+  const payload = context.payload as Payload;
+  const issue = payload.issue;
 
   if (!issue?.body) return;
-  const importantWords = await extractImportantWords();
-  const wordCount = importantWords.length;
+  const importantWords = await extractImportantWords(`Issue title: ${issue.title}\nIssue content: ${issue.body}`);
   let fetchDone = false;
   const perPage = 10;
   let curPage = 1;
-  issue;
 
-  while (!fetchDone) {
-    const issues = await listIssuesForRepo("all", perPage, curPage, "created", "asc");
-    for (const iss of issues) {
-      if (iss.body && iss.number != issue.number && !iss.pull_request) {
-        const probability = (countIncludedWords(iss.body, importantWords) * 100.0) / wordCount;
-        if (probability > parseInt(process.env.SIMILARITY_THRESHOLD || "80")) {
-          if (issue.number) {
-            await upsertCommentToIssue(issue.number, `Similar issue (${iss.title}) found at ${iss.html_url}.\nSimilarity is about ${probability}%`, "created");
-            return;
+  for (const importantWord of importantWords) {
+    try {
+      while (!fetchDone) {
+        const response = await context.octokit.rest.search.issuesAndPullRequests({
+          q: `${importantWord} repo:${payload.repository.owner.login}/${payload.repository.name} is:issue`,
+          sort: "created",
+          order: "desc",
+          per_page: perPage,
+          page: curPage,
+        });
+        if (response.data.items.length > 0) {
+          for (const result of response.data.items) {
+            if (!result.body) return;
+            if (result.id === issue.id) return;
+            const similarity = await measureSimilarity(
+              `Issue title: ${issue.title}\nIssue content: ${issue.body}`,
+              `Issue title: ${result.title}\nIssue content: ${result.body}`
+            );
+            if (similarity > parseInt(process.env.SIMILARITY_THRESHOLD || "80")) {
+              await upsertCommentToIssue(
+                issue.number,
+                `Similar issue (${result.title}) found at ${result.html_url}.\nSimilarity is about ${similarity}%`,
+                "created"
+              );
+              return;
+            }
           }
         }
+        if (response.data.items.length < perPage) fetchDone = true;
+        else curPage++;
       }
+    } catch (e: unknown) {
+      logger.debug(`Could not find any issues, reason: ${e}`);
     }
-    if (issues.length < perPage) fetchDone = true;
-    else curPage++;
   }
 };
--- src/helpers/similarity.ts
+++ src/helpers/similarity.ts
@@ -1,27 +1,44 @@
-import { getBotContext, getLogger } from "../bindings";
-import { Payload } from "../types";
+import { getLogger } from "../bindings";
 import axios from "axios";
 import { ajv } from "../utils";
 import { Static, Type } from "@sinclair/typebox";
 
-export const countIncludedWords = (inputString: string, words: string[]): number => {
-  const body = inputString.toLocaleLowerCase();
-  let count = 0;
-  for (const word of words) {
-    if (body.includes(word.toLowerCase())) {
-      count++;
-    }
-  }
-
-  return count;
+export const extractImportantWords = async (content: string): Promise<string[]> => {
+  const res = await getAnswerFromChatGPT(
+    `${
+      process.env.CHATGPT_SYSTEM_PROMPT_FOR_IMPORTANT_WORDS ||
+      "You are an 'important words finder'. You need to find important words from given context. You only have to give important words from given context and you have to separate the words by #"
+    }`,
+    `${
+      process.env.CHATGPT_USER_PROMPT_FOR_IMPORTANT_WORDS ||
+      "I need your help to find duplicate issues on my GitHub repository. For context, the entire strategy is the following:\n\n1. A new issue is posted\n2. We ask you to extract a word list of the most \"important\" (i.e. unique adjectives?) words.\n3. We search the repository for all issues with the important words.\n4. We go from highest issue number (most recent) and read the issue description.\n5. If >80% confidence that it's a redundant issue, stop the search and link back to it with a warning saying that it's likely to be a duplicate.\nRight now, we are on step 2.\n"
+    } '${content}'`,
+    parseFloat(process.env.IMPORTANT_WORDS_AI_TEMPERATURE || "0")
+  );
+  return res.split("#");
 };
 
-export const extractImportantWords = async (): Promise<string[]> => {
-  const { payload: _payload } = getBotContext();
-  const issue = (_payload as Payload).issue;
-  if (!issue?.body) return [];
-  const res = await getAnswerFromChatGPT(`'Issue title: "${issue.title}"\nIssue body: "${issue.body}"'`);
-  return res.split("#");
+export const measureSimilarity = async (first: string, second: string): Promise<number> => {
+  const res = await getAnswerFromChatGPT(
+    `${
+      process.env.CHATGPT_SYSTEM_PROMPT_FOR_MEASURE_SIMILARITY ||
+      "You are a 'similarity measurer'. Give percent in number. (e.g. 30%)"
+    }`,
+    `
+      ${
+        (process.env.CHATGPT_USER_PROMPT_FOR_MEASURE_SIMILARITY ||
+        'I have two github issues.\nOne is "%first"%\nand\nother is "%second"%Please give me the possibility of the 2 issues are the same content.\n Give me in number format and add % after the number.\nDo not tell other things since I only need the number.')
+          .replace("%first%", first)
+          .replace("%second%", second)
+      }`,
+    parseFloat(process.env.MEASURE_SIMILARITY_AI_TEMPERATURE || "0")
+  );
+  const percent = res.split("%")[0].split(" ").pop();
+  if (!percent) {
+    return 0;
+  } else {
+    return parseFloat(percent);
+  }
 };
 
 const ChatMessageSchema = Type.Object({
@@ -38,27 +55,22 @@ const ChoicesSchema = Type.Object({
 
 type Choices = Static<typeof ChoicesSchema>;
 
-export const getAnswerFromChatGPT = async (prompt: string): Promise<string> => {
+export const getAnswerFromChatGPT = async (systemPrompt: string, userPrompt: string, temperature = 0, max_tokens = 1500): Promise<string> => {
   const logger = getLogger();
   const body = JSON.stringify({
     model: "gpt-3.5-turbo",
     messages: [
       {
         role: "system",
-        content:
-          process.env.CHATGPT_SYSTEM_PROMPT_FOR_IMPORTANT_WORDS ||
-          "You are an 'important words finder'. You need to find important words from given context. You only have to give important words from given context and you have to separate the words by #",
+        content: systemPrompt,
       },
       {
         role: "user",
-        content:
-          (process.env.CHATGPT_USER_PROMPT_FOR_IMPORTANT_WORDS ||
-            "I need your help to find duplicate issues on my GitHub repository. For context, the entire strategy is the following:\n\n1. A new issue is posted\n2. We ask you to extract a word list of the most \"important\" (i.e. unique adjectives?) words.\n3. We search the repository for all issues with the important words.\n4. We go from highest issue number (most recent) and read the issue description.\n5. If >80% confidence that it's a redundant issue, stop the search and link back to it with a warning saying that it's likely to be a duplicate.\nRight now, we are on step 2.\n") +
-          prompt,
+        content: userPrompt,
       },
     ],
-    max_tokens: 1500,
-    temperature: 0,
+    max_tokens,
+    temperature,
     stream: false,
   });
   const config = {
