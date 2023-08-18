760eda6c5f0097f1780bd4b448de32849446a271
DevPanther <techicovery@gmail.com>
2023-08-18 23:18:56 +0100

    chore: setting up functions ts

--- log-webapp/functions/log-engine.ts
+++
@@ -1,9 +0,0 @@
-interface Env {
-  SUPABASE_KEY: string;
-}
-
-export const onRequest: PagesFunction<Env> = async (context) => {
-  //   console.log("running");
-  //   const value = await context.env.SUPABASE_KEY;
-  return new Response("Hi");
-};
