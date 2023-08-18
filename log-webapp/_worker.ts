4bbe1a6c3411a57ae57cd639a403ee34c85d0a81
DevPanther <techicovery@gmail.com>
2023-08-19 00:10:06 +0100

    chore: checking env

--- log-webapp/_worker.ts
+++ log-webapp/_worker.ts
@@ -1,13 +1,17 @@
+import { createClient } from "@supabase/supabase-js";
+
 interface Env {
   ASSETS: Fetcher;
+  SUPABASE_KEY: string;
+  SUPABASE_URL: string;
 }
 
 export default {
   async fetch(request: Request, env: Env) {
     const url = new URL(request.url);
-    if (url.pathname.startsWith("/api/")) {
+    if (url.pathname === "/log-engine") {
       // TODO: Add your custom /api/* logic here.
-      return new Response("Ok");
+      return new Response(env.SUPABASE_KEY);
     }
     // Otherwise, serve the static assets.
     // Without this, the Worker will error and no assets will be served.
