a5b1d88bc16370eee86e4c8a0328b116886b64b9
DevPanther <techicovery@gmail.com>
2023-08-18 23:54:16 +0100

    chore: finalize function setup

--- log-webapp/_worker.ts
+++ log-webapp/_worker.ts
@@ -1,14 +1,16 @@
+interface Env {
+  ASSETS: Fetcher;
+}
+
 export default {
-  async fetch(request, env)
-  {
+  async fetch(request: Request, env: Env) {
     const url = new URL(request.url);
-    if (url.pathname.startsWith('/api/'))
-    {
+    if (url.pathname.startsWith("/api/")) {
       // TODO: Add your custom /api/* logic here.
-      return new Response('Ok');
+      return new Response("Ok");
     }
     // Otherwise, serve the static assets.
     // Without this, the Worker will error and no assets will be served.
     return env.ASSETS.fetch(request);
   },
-}
+};
