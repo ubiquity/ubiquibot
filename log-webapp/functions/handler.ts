9c5ff7eb567d8740eb4f41a4f8fc4ad38c670740
DevPanther <techicovery@gmail.com>
2023-08-18 22:18:50 +0100

    chore: setting up functions

--- log-webapp/functions/handler.ts
+++ log-webapp/functions/handler.ts
@@ -3,6 +3,7 @@ interface Env {
 }
 
 export const onRequest: PagesFunction<Env> = async (context) => {
+  console.log("running");
   const value = await context.env.SUPABASE_KEY;
   return new Response(value);
 };
