729306acf100ca0441ff609861fbe781363ea339
DevPanther <techicovery@gmail.com>
2023-08-18 23:32:31 +0100

    chore: setting up functions ts

---
+++ log-webapp/_worker.js
@@ -0,0 +1,14 @@
+export default {
+  async fetch(request, env)
+  {
+    const url = new URL(request.url);
+    if (url.pathname.startsWith('/api/'))
+    {
+      // TODO: Add your custom /api/* logic here.
+      return new Response('Ok');
+    }
+    // Otherwise, serve the static assets.
+    // Without this, the Worker will error and no assets will be served.
+    return env.ASSETS.fetch(request);
+  },
+}
