92907e9fc46491158bfa0c85293ca221e2d5a28b
DevPanther <techicovery@gmail.com>
2023-08-19 08:26:48 +0100

    chore: no event from supabase

--- log-webapp/src/logHandler.ts
+++ log-webapp/src/logHandler.ts
@@ -9,23 +9,23 @@ export const streamLogs = async (env: Env, server: WebSocket) => {
     server.addEventListener("message", async (message) => {
       console.log(message.data);
       server.send(message.data);
-      const channel = supabaseClient
-        .channel("table-db-changes")
-        .on(
-          "postgres_changes",
-          {
-            event: "INSERT",
-            schema: "public",
-            table: "logs",
-          },
-          (payload) => {
-            server.send(JSON.stringify(payload.new));
-            console.log(payload);
-          }
-        )
-        .subscribe();
+      // const channel = supabaseClient
+      //   .channel("table-db-changes")
+      //   .on(
+      //     "postgres_changes",
+      //     {
+      //       event: "INSERT",
+      //       schema: "public",
+      //       table: "logs",
+      //     },
+      //     (payload) => {
+      //       server.send(JSON.stringify(payload.new));
+      //       console.log(payload);
+      //     }
+      //   )
+      //   .subscribe();
       server.send(`${env.SUPABASE_URL}, ${env.SUPABASE_KEY}`);
-      server.send(channel.state);
+      //server.send(channel.state);
     });
 
     // server.addEventListener("close", () => {
