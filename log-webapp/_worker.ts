4e091c6bbd243db503cb58b85747d15d45297dd2
DevPanther <techicovery@gmail.com>
2023-08-19 06:01:28 +0100

    chore: ws connection on html

--- log-webapp/_worker.ts
+++ log-webapp/_worker.ts
@@ -5,8 +5,19 @@ export default {
   async fetch(request: Request, env: Env) {
     const url = new URL(request.url);
     if (url.pathname === "/log-engine") {
-      // Respond to the original request with a WebSocket URL
-      return streamLogs(env, request);
+      // TODO: Add your custom /api/* logic here.
+      const upgradeHeader = request.headers.get("Upgrade");
+      if (upgradeHeader !== "websocket") {
+        return new Response("Expected websocket", { status: 400 });
+      }
+
+      const [client, server] = Object.values(new WebSocketPair());
+      await streamLogs(env, server);
+
+      return new Response(null, {
+        status: 101,
+        webSocket: client,
+      });
     }
     // Otherwise, serve the static assets.
     // Without this, the Worker will error and no assets will be served.
--- log-webapp/src/logHandler.ts
+++ log-webapp/src/logHandler.ts
@@ -1,43 +1,32 @@
 import { createClient } from "@supabase/supabase-js";
 import { Env } from "../types/global";
-import { WebSocketServer } from "ws";
 
-export const streamLogs = async (env: Env, request: Request) => {
+export const streamLogs = async (env: Env, server: WebSocket) => {
   try {
-    const upgradeHeader = request.headers.get("Upgrade");
+    server.accept();
 
-    if (!upgradeHeader || upgradeHeader !== "websocket") {
-      return new Response("Expected Upgrade: websocket", { status: 426 });
-    }
-
-    const wss = new WebSocketServer({ port: 8080 });
-
-    wss.on("connection", function connection(ws) {
-      ws.on("message", function message(data) {
-        console.log("received: %s", data);
-      });
-
-      ws.send("something");
+    server.addEventListener("message", async (message) => {
+      console.log(message);
     });
 
-    // const supabaseClient = createClient(env.SUPABASE_KEY, env.SUPABASE_URL);
+    const supabaseClient = createClient(env.SUPABASE_KEY, env.SUPABASE_URL);
 
-    // const channel = supabaseClient
-    //   .channel("table-db-changes")
-    //   .on(
-    //     "postgres_changes",
-    //     {
-    //       event: "INSERT",
-    //       schema: "public",
-    //       table: "logs",
-    //     },
-    //     (payload) => server.send(JSON.stringify(payload))
-    //   )
-    //   .subscribe();
+    const channel = supabaseClient
+      .channel("table-db-changes")
+      .on(
+        "postgres_changes",
+        {
+          event: "INSERT",
+          schema: "public",
+          table: "logs",
+        },
+        (payload) => server.send(JSON.stringify(payload))
+      )
+      .subscribe();
 
-    // server.addEventListener("close", () => {
-    //   channel.unsubscribe();
-    // });
+    server.addEventListener("close", () => {
+      channel.unsubscribe();
+    });
   } catch (e) {
     console.log(e);
   }
