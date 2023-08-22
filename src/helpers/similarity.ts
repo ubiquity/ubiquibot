57b979180cf2bddd730ab04728e499a70c19bee6
byteballet <shaorun1010@gmail.com>
2023-08-21 22:48:00 -0700

    update: log

--- src/helpers/similarity.ts
+++ src/helpers/similarity.ts
@@ -96,6 +96,7 @@ export const getAnswerFromChatGPT = async (systemPrompt: string, userPrompt: str
       return "";
     }
     const answer = choice[0].message.content;
+    console.log(answer);
     return answer;
   } catch (error) {
     logger.error(`Getting response from ChatGPT failed: ${error}`);
