2c66147163efd65e351257ad9a0f21cbf5d87598
DevPanther <techicovery@gmail.com>
2023-08-22 07:42:53 +0100

    chore: logger setup done

--- log-app/scripts/index.ts
+++ log-app/scripts/index.ts
@@ -1,5 +1,39 @@
-import { getLevelString } from "./helpers/utils";
 import { Logs } from "./types/log";
+
+enum Level {
+  ERROR = "error",
+  WARN = "warn",
+  INFO = "info",
+  HTTP = "http",
+  VERBOSE = "verbose",
+  DEBUG = "debug",
+  SILLY = "silly",
+}
+
+const createGitHubCommentURL = (orgName: string, repoName: string, issueNumber: number, commentId: number) => {
+  return `https://github.com/${orgName}/${repoName}/issues/${issueNumber}#issuecomment-${commentId}`;
+};
+
+const getLevelString = (level: number) => {
+  switch (level) {
+    case 0:
+      return Level.ERROR;
+    case 1:
+      return Level.WARN;
+    case 2:
+      return Level.INFO;
+    case 3:
+      return Level.HTTP;
+    case 4:
+      return Level.VERBOSE;
+    case 5:
+      return Level.DEBUG;
+    case 6:
+      return Level.SILLY;
+    default:
+      return -1; // Invalid level
+  }
+};
 
 const filterSelect = document.getElementById("filter") as unknown as HTMLSelectElement;
 const clearButton = document.getElementById("clear") as HTMLButtonElement;
@@ -43,12 +77,14 @@ const updateLogTable = () => {
 
   logBody.innerHTML = "";
   filteredLogs.forEach((log) => {
+    const commentUrl = createGitHubCommentURL(log.org_name, log.repo_name, log.issue_number, log.comment_id);
     const row = document.createElement("tr");
     row.innerHTML = `
-        <td>${log.timestamp}</td>
-        <td>${getLevelString(log.level)}</td>
         <td>${log.log_message}</td>
-        `;
+        <td>${getLevelString(log.level)}</td>
+        <td>${log.timestamp}</td>
+        <td><a href="${commentUrl}">Comment - ${log.comment_id}</a></td>
+    `;
     logBody.appendChild(row);
   });
 };
--- log-app/scripts/types/log.ts
+++ log-app/scripts/types/log.ts
@@ -2,4 +2,8 @@ export type Logs = {
   timestamp: string;
   log_message: string;
   level: number;
+  repo_name: string;
+  org_name: string;
+  comment_id: number;
+  issue_number: number;
 };
