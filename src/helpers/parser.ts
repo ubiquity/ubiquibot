83880160cc8a9d2111adfc7e6fc8ac5be67d389b
Seprintour <godstimeoobasi@gmail.com>
2023-09-06 03:04:50 +0100

    fix: close pr issues and stop running handlers twice

--- src/helpers/parser.ts
+++ src/helpers/parser.ts
@@ -7,21 +7,33 @@ interface GitParser {
   issue_number: number;
 }
 
-export const gitIssueParser = async ({ owner, repo, issue_number }: GitParser): Promise<boolean> => {
+export const gitIssueParser = async ({ owner, repo, issue_number }: GitParser): Promise<{ number: number; href: string }[]> => {
   try {
     const { data } = await axios.get(`https://github.com/${owner}/${repo}/issues/${issue_number}`);
     const dom = parse(data);
     const devForm = dom.querySelector("[data-target='create-branch.developmentForm']") as HTMLElement;
     const linkedPRs = devForm.querySelectorAll(".my-1");
-    if (linkedPRs.length > 0) {
-      //has LinkedPRs
-      return true;
-    } else {
-      //no LinkedPRs
-      return false;
-    }
+
+    const prs: { number: number; href: string }[] = [];
+    linkedPRs.forEach((pr) => {
+      const prUrl = pr.querySelector("a")?.attrs?.href;
+
+      if (!prUrl) return;
+
+      const prInfo = prUrl.split("/");
+      const prNumber = Number(prInfo[prInfo.length - 1]);
+      const prHref = `https://github.com${prUrl}`;
+
+      prs.push({ number: prNumber, href: prHref });
+    });
+    return prs;
   } catch (error) {
-    return true;
+    return [
+      {
+        number: 0,
+        href: "",
+      },
+    ];
   }
 };
 
