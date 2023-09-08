9d4aa0c89c1412bf368f166ebf1dd827123ca242
0xcodercrane <108444211+0xcodercrane@users.noreply.github.com>
2023-09-08 12:02:34 +0800

    feat: better setup

--- test/utils/address.spec.ts
+++ test/utils/address.spec.ts
@@ -8,7 +8,7 @@ describe("utils:address", () => {
       const addr = "0xd8da6bf26964af9d7eed9e03e53415d37aa96045"; // vitalik.eth
       // 1. length is greater than 13
       expect(shortenEthAddress(addr, 15)).to.be.eq("0xd8da6b...aa96045");
-      expect(shortenEthAddress(addr, 14)).to.be.eq("0xd8da6...aa96045");
+      expect(shortenEthAddress(addr, 14)).to.be.eq("0xd8da6b...a96045");
 
       // 2. length is less than/equal to 13
       expect(shortenEthAddress(addr, 10)).to.be.eq("0xd8da...96045");
