1329f12fcb691077e9d71ea1f80d559c71a20687
Keyrxng <106303466+Keyrxng@users.noreply.github.com>
2023-09-09 08:52:24 +0100

    fix: table comment
    fix table so no whitespace

--- src/handlers/comment/handlers/table.ts
+++ src/handlers/comment/handlers/table.ts
@@ -16,24 +16,23 @@ export const tableComment = ({
   days?: number;
 }) => {
   return `
-<code>
-  <table>
+  <html>
+  <body>
+  <!--StartFragment-->
+
   ${
     isBountyStale
-      ? `<tr><td>Warning!</td> <td>This task was created over ${days} days ago. Please confirm that this issue specification is accurate before starting.</td></tr>`
+      ? `| Warning! | This task was created over ${days} days ago. Please confirm that this issue specification is accurate before starting. |\n |--|--|`
       : ``
   }
-  <tr>
-    <td>Deadline</td>
-    <td>${deadline}</td>
-  </tr>
-  <tr>
-    <td>Registered Wallet</td>
-    <td>${wallet}</td>
-  </tr>
-  ${multiplier ? `<tr><td>Payment Multiplier</td><td>${multiplier}</td></tr>` : ``}
-  ${reason ? `<tr><td>Multiplier Reason</td><td>${reason}</td></tr>` : ``}
-  ${bounty ? `<tr><td>Total Bounty</td><td>${bounty}</td></tr>` : ``}
-  </table>
-</code>`;
+  ${!isBountyStale ? `| Deadline | ${deadline} |\n |--|--|` : `| Deadline | ${deadline} |`}
+  | Registered Wallet | ${wallet} |
+  ${multiplier ? `| Payment Multiplier | ${multiplier} |` : ``}
+  ${reason ? `| Multiplier Reason | ${reason} |` : ``}
+  ${bounty ? `| Total Bounty | ${bounty} |` : ``}
+    
+  <!--EndFragment-->
+  </body>
+  </html>
+`;
 };
