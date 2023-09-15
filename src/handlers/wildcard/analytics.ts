38b16a67ab2bf61bdc02193da18ba6dd6d196348
me505 <62057938+me505@users.noreply.github.com>
2023-09-15 13:08:58 +0000

    feat: price override

--- src/handlers/wildcard/analytics.ts
+++ src/handlers/wildcard/analytics.ts
@@ -2,7 +2,6 @@ import { getMaxIssueNumber, upsertIssue, upsertUser } from "../../adapters/supab
 import { getBotConfig, getLogger } from "../../bindings";
 import { listIssuesForRepo, getUser, calculateWeight } from "../../helpers";
 import { Issue, IssueType, User, UserProfile } from "../../types";
-import { getTargetPriceLabel } from "../shared";
 
 /**
  * Checks the issue whether it's a bounty for hunters or an issue for not
@@ -18,7 +17,7 @@ export const bountyInfo = (
   priceLabel: string | undefined;
 } => {
   const config = getBotConfig();
-  const labels = issue.labels;
+
   const timeLabels = config.price.timeLabels.filter((item) => labels.map((i) => i.name).includes(item.name));
   const priorityLabels = config.price.priorityLabels.filter((item) => labels.map((i) => i.name).includes(item.name));
 
@@ -27,7 +26,14 @@ export const bountyInfo = (
   const minTimeLabel = timeLabels.length > 0 ? timeLabels.reduce((a, b) => (calculateWeight(a) < calculateWeight(b) ? a : b)).name : undefined;
   const minPriorityLabel = priorityLabels.length > 0 ? priorityLabels.reduce((a, b) => (calculateWeight(a) < calculateWeight(b) ? a : b)).name : undefined;
 
-  const priceLabel = getTargetPriceLabel(minTimeLabel, minPriorityLabel);
+  const labels = issue.labels;
+  const labelNames = labels.map((i) => i.name);
+  let priceLabel: string = "";
+  labelNames.forEach((e) => {
+    if (e.includes("Price")) {
+      priceLabel = e;
+    }
+  });
 
   return {
     isBounty,
