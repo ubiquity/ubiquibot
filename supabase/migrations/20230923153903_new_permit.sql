bfc9787351ffeb0e92f98362eb56e4a6a73c0b16
Seprintour <godstimeoobasi@gmail.com>
2023-09-23 19:23:45 +0100

    chore: create permit table and move data

---
+++ supabase/migrations/20230923153903_new_permit.sql
@@ -0,0 +1,48 @@
+CREATE TABLE IF NOT EXISTS
+  new_permits (
+    id serial primary key,
+    created_at timestamptz not null,
+    updated_at timestamptz not null,
+    network smallserial not null,
+    token char(42) not null,
+    amount numeric not null,
+    nonce numeric not null,
+    deadline numeric not null,
+    beneficiary char(42) not null,
+    owner char(42) not null,
+    signature char(132) not null
+  );
+
+INSERT INTO
+  new_permits (
+    id,
+    created_at,
+    updated_at,
+    network,
+    token,
+    amount,
+    nonce,
+    deadline,
+    beneficiary,
+    owner,
+    signature
+  )
+SELECT
+  id,
+  created_at,
+  created_at,
+  network_id,
+  token_address,
+  CAST(payout_amount AS numeric),
+  CAST(nonce AS numeric),
+  CAST(deadline AS numeric),
+  bounty_hunter_address,
+  wallet_owner_address,
+  signature
+FROM
+  permits;
+
+DROP TABLE permits;
+
+ALTER TABLE new_permits
+RENAME TO permits;
