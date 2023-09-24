8ba4743f722e5469bb2d42d72a2a81fe978f4179
whilefoo <whilefoo@users.noreply.github.com>
2023-09-24 16:24:11 +0200

    feat: longer delays

--- tests/utils.ts
+++ tests/utils.ts
@@ -11,13 +11,13 @@ export function waitForNWebhooks(n = 1) {
 
     const timer = setTimeout(() => {
       reject(new Error(`timeout, received ${i} webhooks, expected ${n}`));
-    }, 10000);
+    }, 30000);
 
     webhookEventEmitter.on("event", () => {
       i += 1;
       if (i == n) {
         clearTimeout(timer);
-        setTimeout(resolve, 500);
+        setTimeout(resolve, 1000);
       }
     });
   });
