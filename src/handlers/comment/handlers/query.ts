2c6bdb59a26a725d6b27081bcb0fba702169b41c
アレクサンダー.eth <gpg@pavlovcik.com>
2023-10-06 06:35:17 +0900

    refactor(query): trying to go quickly to get things stable

--- src/handlers/comment/handlers/query.ts
+++ src/handlers/comment/handlers/query.ts
@@ -1,63 +1,65 @@
 // import { getAllAccessLevels, getWalletInfo, upsertAccessControl } from "../../../adapters/supabase";
-import { getBotContext, getLogger } from "../../../bindings";
+import { getAdapters, getBotContext, getLogger } from "../../../bindings";
 import { Payload } from "../../../types";
-import { ErrorDiff } from "../../../utils/helpers";
 
-export const query = async (body: string) => {
+export async function query(body: string) {
   const context = getBotContext();
   const logger = getLogger();
   const payload = context.payload as Payload;
   const sender = payload.sender.login;
-  const { repository, organization } = payload;
+  // const { repository, organization } = payload;
 
-  const id = organization?.id || repository?.id; // repository?.id as fallback
+  // const id = organization?.id || repository?.id; // repository?.id as fallback
 
   logger.info(`Received '/query' command from user: ${sender}`);
 
   const issue = payload.issue;
-  if (!issue) {
-    logger.info(`Skipping '/query' because of no issue instance`);
-    return `Skipping '/query' because of no issue instance`;
-  }
+  if (!issue) return logger.info(`Skipping '/query' because of no issue instance`);
 
   const regex = /^\/query\s+@([\w-]+)\s*$/;
   const matches = body.match(regex);
-  const user = matches?.[1];
-  const repo = payload.repository;
+  const username = matches?.[1];
+  // const repo = payload.repository;
 
-  if (user) {
-    let data = await getAllAccessLevels(user, repo.full_name);
-    if (!data) {
-      logger.info(`Access info does not exist for @${user}`);
-      try {
-        await upsertAccessControl(user, repo.full_name, "time_access", true);
-        data = {
-          multiplier: false,
-          priority: false,
-          time: true,
-          price: false,
-        };
-      } catch (e) {
-        ErrorDiff(e);
-        return `Error upserting access info for @${user}`;
-      }
-    }
-    const walletInfo = await getWalletInfo(user, id?.toString());
-    if (!walletInfo?.address) {
-      return `Error retrieving multiplier and wallet address for @${user}`;
-    } else {
-      return `@${user}'s wallet address is ${walletInfo?.address}, multiplier is ${walletInfo?.multiplier} and access levels are
+  if (username) {
+    // Get access levels from the access table
+    const database = getAdapters().supabase;
+    // let accessData = await getAccessLevels(user, repo.full_name, id);
 
-| access type | access level        |
-| ----------- | ------------------- |
-| multiplier  | ${data.multiplier}  |
-| priority    | ${data.priority}    |
-| time        | ${data.time}        |
-| price       | ${data.price}       |
-      `;
+    // lookup user object from github api based on the username
+    const { data: user } = await context.octokit.users.getByUsername({ username });
+    if (!user) return logger.error(`No user found for username: ${username}`);
+
+    const accessData = await database.access.getAccess(user.id);
+
+    // Get wallet info from the users and wallets tables
+    const walletAddress = await database.wallet.getAddress(user.id);
+
+    let resultMessage = "";
+
+    if (!accessData && !walletAddress) {
+      return `No access or wallet information is set for @${user}`;
     }
-  } else {
-    logger.error("Invalid body for query command");
-    return `Invalid syntax for query command \n usage /query @user`;
+
+    if (accessData) {
+      const { multiplier, multiplier_reason, labels, location_id } = accessData;
+      // const locationInfo = await await database.access.getAccessLocation(location_id);
+      resultMessage += `@${user}'s access levels are set as follows:\n
+  Multiplier: ${multiplier}\n
+  Reason: ${multiplier_reason}\n
+  Labels: ${JSON.stringify(labels)}\n
+  \n\n`; // Original comment: ${locationInfo.node_url}
+    }
+
+    if (walletAddress) {
+      // const { address, location_id } = walletData;
+      // const locationInfo = await getLocationById(location_id);
+      resultMessage += `@${user}'s wallet information is as follows:\n
+  Address: ${walletAddress}\n`; //Original comment: ${locationInfo.node_url}\n
+    }
+
+    return resultMessage;
   }
-};
+
+  return logger.error("Invalid body for query command \n usage /query @user");
+}
