216ec553935b17c4c25dfdb16d2edbf91184b492
アレクサンダー.eth <gpg@pavlovcik.com>
2023-10-19 00:25:31 +0900

    feat: short files
    contributors should modularize code

--- src/handlers/comment/handlers/issue/calculate-quality-score.ts
+++ src/handlers/comment/handlers/issue/calculate-quality-score.ts
@@ -1,46 +1,13 @@
 import { Comment, Issue } from "../../../../types/payload";
 // import OpenAI from "openai";
 import { encodingForModel } from "js-tiktoken";
-import util from "util";
 
 const botCommandsAndCommentsFilter = (comment) => !comment.body.startsWith("/") && comment.user.type === "User";
 
 export function calculateQualScore(issue: Issue, comments: Comment[]) {
-  const gpt3TurboEncoder = encodingForModel("gpt-3.5-turbo");
-  // let score = 0;
-  const specificationComment = issue.body;
-  if (!specificationComment) {
-    throw new Error("Issue specification comment is missing");
-  }
-
-  const contributorCommentsWithTokens = comments.filter(botCommandsAndCommentsFilter).map((comment) => {
-    return {
-      tokens: gpt3TurboEncoder.encode(comment.body),
-      comment,
-    };
-  });
-
-  const contributorCommentsTokens = contributorCommentsWithTokens.map(({ tokens }) => tokens);
-  const sumOfContributorTokens = contributorCommentsWithTokens.reduce((acc, { tokens }) => acc + tokens.length, 0);
-  const specificationTokens = gpt3TurboEncoder.encode(specificationComment);
-  const sumOfSpecificationTokens = specificationTokens.length;
-  const totalSumOfTokens = sumOfSpecificationTokens + sumOfContributorTokens;
-  const estimatedOptimalModel = estimateOptimalModel(totalSumOfTokens);
-  // const decodeTest = gpt3TurboEncoder.decode([
-  //   -25, -24, -23, -22, -21, -20, -19, -18, -17, -16, -15, -14, -13, -12, -11, -10, -9, -8, -7, -6, -5, -4, -3, -2, -1,
-  //   0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25,
-  // ]);
-  const buffer = {
-    // decodeTest,
-    specificationTokens,
-    contributorCommentsTokens,
-    sumOfContributorTokens,
-    sumOfSpecificationTokens,
-    totalSumOfTokens,
-    estimatedOptimalModel,
-  };
-  console.trace(util.inspect(buffer, { depth: null }));
-  // return score;
+  const tokens = countTokensOfConversation(issue, comments);
+  const estimatedOptimalModel = estimateOptimalModel(tokens);
+  return { tokens, estimatedOptimalModel };
 }
 
 function estimateOptimalModel(sumOfTokens: number) {
@@ -56,6 +23,35 @@ function estimateOptimalModel(sumOfTokens: number) {
     console.warn("Backup plan for development purposes only, but using gpt-4-32k due to huge context size");
     return "gpt-4-32k";
   }
+}
+
+function countTokensOfConversation(issue: Issue, comments: Comment[]) {
+  const gpt3TurboEncoder = encodingForModel("gpt-3.5-turbo");
+  const specificationComment = issue.body;
+  if (!specificationComment) {
+    throw new Error("Issue specification comment is missing");
+  }
+
+  const contributorCommentsWithTokens = comments.filter(botCommandsAndCommentsFilter).map((comment) => {
+    return {
+      tokens: gpt3TurboEncoder.encode(comment.body),
+      comment,
+    };
+  });
+
+  // const contributorCommentsTokens = contributorCommentsWithTokens.map(({ tokens }) => tokens);
+  const sumOfContributorTokens = contributorCommentsWithTokens.reduce((acc, { tokens }) => acc + tokens.length, 0);
+  const specificationTokens = gpt3TurboEncoder.encode(specificationComment);
+  const sumOfSpecificationTokens = specificationTokens.length;
+  const totalSumOfTokens = sumOfSpecificationTokens + sumOfContributorTokens;
+  // const estimatedOptimalModel = estimateOptimalModel(totalSumOfTokens);
+
+  // const buffer = {
+  //   totalSumOfTokens,
+  //   estimatedOptimalModel,
+  // };
+  // return buffer;
+  return totalSumOfTokens;
 }
 
 // async function gpt() {
--- src/handlers/comment/handlers/issue/issue-closed.ts
+++ src/handlers/comment/handlers/issue/issue-closed.ts
@@ -145,16 +145,17 @@ type Payments = {
 // Your existing logic here
 
 export async function issueClosed() {
-  const { issue } = preamble();
+  const { issue, logger } = preamble();
   const issueComments = await getAllIssueComments(issue.number);
-  calculateQualScore(issue, issueComments);
+  return logger.ok("Issue closed. Calculating quality score.", calculateQualScore(issue, issueComments));
 }
 
 function preamble() {
   const runtime = Runtime.getState();
   const context = runtime.latestEventContext;
+  const logger = runtime.logger;
   const payload = context.payload as Payload;
   const issue = payload.issue as Issue;
   if (!issue) throw runtime.logger.error("Issue is not defined");
-  return { issue, payload, context, runtime };
+  return { issue, payload, context, runtime, logger };
 }
