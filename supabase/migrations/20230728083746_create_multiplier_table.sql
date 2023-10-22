dd14ed2c01e18f99a489f70f028d0e50c8e19132
Paul <web3samuraidev@gmail.com>
2023-10-22 20:59:46 +0000

    test: remove multiplier table and migration

--- supabase/migrations/20230728083746_create_multiplier_table.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE multiplier (
-    user_id character varying(255) NOT NULL PRIMARY KEY,
-    value numeric,
-    reason text,
-    created_at TIMESTAMP WITHOUT TIME ZONE,
-    updated_at TIMESTAMP WITHOUT TIME ZONE
-);
