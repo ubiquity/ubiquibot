713f4da0d5fce86c693e0a46a38ee7e8dc9543c2
Paul <web3samuraidev@gmail.com>
2023-10-22 16:16:34 +0000

    test: update primary key

--- supabase/migrations/20230207011749_create_wallet_table.sql
+++ supabase/migrations/20230207011749_create_wallet_table.sql
@@ -2,6 +2,6 @@ CREATE TABLE wallets (
     id SERIAL PRIMARY KEY,
     created_at text,
     updated_at text
-    user_name character varying(255) NOT NULL PRIMARY KEY,
+    user_name character varying(255) NOT NULL,
     wallet_address character(42),
 );
