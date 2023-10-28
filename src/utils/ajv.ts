bcb35cd9a81e5577a8a2b36a848384aa4a5d6455
アレクサンダー.eth <gpg@pavlovcik.com>
2023-10-28 17:24:11 +0900

    chore(ajv): had to disable strict validation no idea what the origin of the issue is

--- src/utils/ajv.ts
+++ src/utils/ajv.ts
@@ -1,32 +1,37 @@
 import Ajv, { Schema } from "ajv";
 import addFormats from "ajv-formats";
 
-export const ajv = addFormats(new Ajv(), {
-  formats: [
-    "date",
-    "time",
-    "date-time",
-    "duration",
-    "uri",
-    "uri-reference",
-    "uri-template",
-    "email",
-    "hostname",
-    "ipv4",
-    "ipv6",
-    "regex",
-    "uuid",
-    "json-pointer",
-    "relative-json-pointer",
-    "byte",
-    "int32",
-    "int64",
-    "float",
-    "double",
-    "password",
-    "binary",
-  ],
-});
+export const ajv = addFormats(
+  new Ajv({
+    strict: false,
+  }),
+  {
+    formats: [
+      "date",
+      "time",
+      "date-time",
+      "duration",
+      "uri",
+      "uri-reference",
+      "uri-template",
+      "email",
+      "hostname",
+      "ipv4",
+      "ipv6",
+      "regex",
+      "uuid",
+      "json-pointer",
+      "relative-json-pointer",
+      "byte",
+      "int32",
+      "int64",
+      "float",
+      "double",
+      "password",
+      "binary",
+    ],
+  }
+);
 
 function getAdditionalProperties() {
   return ajv.errors
