4cbbf38b16474b2dc5b8c146c1db01cf2a510f62
アレクサンダー.eth <gpg@pavlovcik.com>
2023-11-21 10:37:15 +0900

    chore: delete ubiquibot-config-default.ts and modify generateConfiguration function
    https://github.com/pavlovcik/ubiquibot/commit/ca1d827aa45c16f41e856040fc65a38d5145f1ab#r132819581

--- src/utils/generate-configuration.ts
+++ src/utils/generate-configuration.ts
@@ -5,7 +5,6 @@ import { Context as ProbotContext } from "probot";
 import YAML from "yaml";
 import Runtime from "../bindings/bot-runtime";
 import { BotConfig, Payload, stringDuration, validateBotConfig } from "../types";
-import ubiquibotConfigDefault from "../ubiquibot-config-default";
 
 const UBIQUIBOT_CONFIG_REPOSITORY = "ubiquibot-config";
 const UBIQUIBOT_CONFIG_FULL_PATH = ".github/ubiquibot-config.yml";
@@ -29,7 +28,7 @@ export async function generateConfiguration(context: ProbotContext): Promise<Bot
     })
   );
 
-  const merged = mergeWith(ubiquibotConfigDefault, orgConfig, repoConfig, (objValue: unknown, srcValue: unknown) => {
+  const merged = mergeWith({}, orgConfig, repoConfig, (objValue: unknown, srcValue: unknown) => {
     if (Array.isArray(objValue) && Array.isArray(srcValue)) {
       // if it's string array, concat and remove duplicates
       if (objValue.every((value) => typeof value === "string")) {
