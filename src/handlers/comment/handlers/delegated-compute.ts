b253789b4f2857c7b4ae7b80a968b5b661b34c51
アレクサンダー.eth <gpg@pavlovcik.com>
2023-12-21 10:20:16 +0900

    fix: dispatcher use current organization

--- src/handlers/comment/handlers/delegated-compute.ts
+++ src/handlers/comment/handlers/delegated-compute.ts
@@ -10,25 +10,26 @@ export interface DelegatedComputeInputs {
 }
 
 export const computeLocation = {
-  // TODO: Make this configurable
-  owner: "ubiquity",
+  //   // TODO: Make this configurable
+  owner: null, //  "ubiquity",
   repository: "ubiquibot-config",
-  workflow: "compute.yml",
-};
+  workflowId: "compute.yml",
+} as { owner: null | string; repository: string; workflowId: string };
 
 export async function delegateCompute(context: Context, inputs: DelegatedComputeInputs) {
-  const owner = context.payload?.organization?.login || context.payload.repository.owner.login;
-  const repo = computeLocation.repository;
-  const workflowId = computeLocation.workflow;
-  const ref = await getDefaultBranch(context);
+  computeLocation.owner = context.payload?.organization?.login || context.payload.repository.owner.login;
+  if (!computeLocation.owner) {
+    throw context.logger.error("Compute repository owner is not defined");
+  }
+  const ref = await getDefaultBranch(context, computeLocation.owner, computeLocation.repository);
 
   // You must authenticate using an access token with the repo scope to use this endpoint. GitHub Apps must have the actions:write permission to use this endpoint.
   // For more information, see "Creating a personal access token for the command line."
   // https://docs.github.com/en/authentication/keeping-your-account-and-data-secure/managing-your-personal-access-tokens
   const response = await context.octokit.actions.createWorkflowDispatch({
-    owner,
-    repo,
-    workflow_id: workflowId,
+    owner: computeLocation.owner,
+    repo: computeLocation.repository,
+    workflow_id: computeLocation.workflowId,
     ref,
     inputs: inputs as unknown as { [key: string]: string },
   });
@@ -37,10 +38,10 @@ export async function delegateCompute(context: Context, inputs: DelegatedCompute
   }
 }
 
-async function getDefaultBranch(context: Context) {
+async function getDefaultBranch(context: Context, owner: string, repository: string) {
   const computeRepositoryFull = await context.octokit.repos.get({
-    owner: computeLocation.owner,
-    repo: computeLocation.repository,
+    owner: owner,
+    repo: repository,
   });
   return computeRepositoryFull.data.default_branch;
 }
